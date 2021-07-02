e76f61008f62a2b721e138ac8eaec102e6716611
Angell <angellbelger@hotmail.com>
2021-07-02 13:09:12 -0300

    update

--- main.c
+++ main.c
@@ -8,12 +8,14 @@ struct people
     char name[30];
     char gender;
     int age;
+    int points;
 };
 
 
 int main()
 {
     struct people card; 
+    card.points = 0;
 
     printf("\nHello, world.\n");
 
@@ -52,6 +54,262 @@ int main()
     printf("Idade: ");
     fflush(stdin);
     scanf("%i", &card.age);
+
+    true = 1;
+    char r;
+
+    printf("\nSr %s, responda o questionario:\n", card.name);
+
+    while ( true == 1 )
+    {
+        printf("\nTem febre [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 5;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem dor de cabeca [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 1;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem secrecao nasal ou espirros [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 1;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem dor/irritacao na garganta [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 1;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem tosse seca [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 3;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem dificuldades respiratorias [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 10;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem dores no corpo [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 1;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nTem diarreia [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 1;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nEsteve em contato, nos ultimos 14 dias, com um caso diagnosticado com COVID-19 [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 10;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    true = 1;
+
+    while ( true == 1)
+    {
+        printf("\nEsteve em locais com grande aglomeracao [S | N]: ");
+        fflush(stdin);
+        scanf("%c", &r);
+        if ( r == 'S' || r == 's')
+        {
+            card.points += 3;
+            true = 0;
+        }
+        else
+        {
+            if ( r == 'N' || r == 'n')
+            {
+                true = 0;
+            }
+            else
+            {
+                printf("Por favor, digite S para sim, e N para nao.\n");
+            }
+        }
+    }
+
+    printf("%i", card.points);
+    
 
 
     return 0;
